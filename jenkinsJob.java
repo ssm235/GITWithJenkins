1b400fe649dd63e58b83d69940cfb1418035cf90
ssm235 <mithari2305@gmail.com>
2017-03-27 22:12:11 +0530

    added succsfully

---
+++ jenkinsJob.java
@@ -0,0 +1,23 @@
+package jenkinswithGIT;
+
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class jenkinsJob {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		WebDriver driver;
+//		
+//		driver=new FirefoxDriver();
+//		
+//		driver.get("https://www.google.com");
+		
+		for (int i=1;i<=10;i++)
+		{
+			System.out.println("Hello: "+i);
+		}
+
+	}
+
+}
